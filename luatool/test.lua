a1061ef552ab8dd033c2df4937db9505565e99d3
markus <git@mknj.de>
2015-09-24 20:05:29 +0000

    added usage example

--- luatool/test.lua
+++ luatool/test.lua
@@ -1,2 +1,7 @@
+-- i.e. 
+-- mosquitto_pub -h $IP -t $ID/test.lua -f test.lua 
+-- mosquitto_pub -h $IP -t $ID/cmd -m 'dofile("test.lua")'
+-- WARNING: filesize must be <1000 bytes
+
 X=422
 m:publish("/uptime","alice"..X,0,0,x)
