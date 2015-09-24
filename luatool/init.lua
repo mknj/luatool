6adb2248fbc64e4158a64b309d9787bb362ea072
markus <git@mknj.de>
2015-09-24 19:33:28 +0000

    put MQTT config into Konfig.lua file

--- luatool/init.lua
+++ luatool/init.lua
@@ -1,40 +1,53 @@
-wifi.setmode(wifi.STATION)
-wifi.sta.config("XXX","XXXX")
-mac="/"..wifi.sta.getmac()
+print("v4")
+dofile("konfig.lua")
+wd=0
+m = mqtt.Client(ID, 120, USER,PW)
 function server()
-t=0
-m = mqtt.Client(mac, 120, "user", "password")
-function x(c) end
-m:connect("10.3.3.96", 1883, 0, function(c) 
-  m:on("offline", function(con) node.restart() end)
-  t=tmr.time()
-  print("connected") 
-  m:subscribe(mac.."/cmd",0, x)
-  m:subscribe(mac.."/tst",0, x)
-  m:on("message", function(c, topic, data) 
-    if data ~= nil then
-      print(topic.." "..string.len(data))
-    end
-    if topic == mac.."/cmd" then
-      node.input(data.."\r\n")
-    end
-    if topic == mac.."/tst" then
-      file.remove("test.lua")
-      f=file.open("test.lua","w")
-      file.write(data)
-      file.close()
-    end
-  end)
-  function p()
-    m:publish("/uptime",mac.." "..node.heap().." "..(tmr.time()-t),0,0,x)
-  end
-  p()
-  tmr.alarm(0, 60000, 1, p)
-end)
+	t=0
+	function x(c) end
+	m:connect(IP, 1883, 0, function(c) 
+		m:on("offline", function(con) node.restart() end)
+		t=tmr.time()
+		print("MQTT ok") 
+		m:subscribe("/uptime",0, x)
+		m:subscribe(ID.."/cmd",0, x)
+		m:subscribe(ID.."/test.lua",0, x)
+		m:on("message", function(c, topic, data) 
+			wd=0
+--			if data ~= nil then
+--				print(topic.." "..string.len(data))
+--			end
+			if topic == ID.."/cmd" then
+				node.input(data.."\r\n")
+			end
+			if topic == ID.."/test.lua" then
+				file.remove("test.lua")
+				f=file.open("test.lua","w")
+				file.write(data)
+				file.close()
+			end
+		end)
+		wd=0
+		p()
+	end)
+	function p()
+		if(wd==1) then
+			node.restart()
+		end
+		wd=1
+		m:publish("/uptime",ID.." "..node.heap().." "..(tmr.time()-t),0,0,x)
+	end
+	tmr.stop(0)
+	tmr.alarm(0, 60000, 1, p)
+	print("wifi ok") 
 end
-tmr.alarm(0,1000,1,function()
-if(wifi.sta.status()==5) then
-tmr.stop(0)
-server()
-end
+tmr.alarm(0,5000,1,function()
+	if(wd>60) then
+		node.restart()
+	end
+	wd=wd+1
+	if(wifi.sta.status()==5) then
+		tmr.stop(0)
+		server()
+	end
 end)
--- luatool/konfig.lua
+++ luatool/konfig.lua
@@ -1,3 +1,7 @@
 print("konfiguriere wlan")
 wifi.setmode(wifi.STATION)
 wifi.sta.config("SID","PASSWORD")
+ID="/"..wifi.sta.getmac()
+IP="YOUR_MQTT_SERVER_IP_ADDRESS"
+USER="mqtt user"
+PW="mqtt password"
